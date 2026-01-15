79b736232ba32e53411189ddcadfd70eb9a8e4f5
Loaf-God7 <brian10420@gmail.com>
2026-01-15 07:24:23 -0500

    Update flash_mainboard.sh

--- scripts/flash_mainboard.sh
+++ scripts/flash_mainboard.sh
@@ -1,2 +1,2 @@
 cd /home/gem/TXv2-Pi-Project/firmware
-python3 /home/gem/klipper/scripts/flash_usb.py -t stm32f072xb -d 0483:df11 -s 0x8000000 txpi250k.bin
+python3 /home/gem/klipper/scripts/flash_usb.py -t stm32f072xb -d 0483:df11 -s 0x8000000 txpi-250k.bin
